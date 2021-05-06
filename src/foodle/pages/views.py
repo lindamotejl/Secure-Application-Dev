27f79e1c54b0f674b5ed242e0fbc41bd5bad1142
KevinD2308 <56120859+KevinD2308@users.noreply.github.com>
2021-05-06 13:36:14 +0200

    Redirecting after signing up
    Now the "sign up" button redirects the user to the homepage.

--- src/foodle/pages/views.py
+++ src/foodle/pages/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from users.forms import UserRegisterForm
 
 # Create your views here.
@@ -15,8 +14,7 @@ def register_view(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('home.html')
+            return redirect('home')
     else:
         form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
--- src/foodle/users/views.py
+++ src/foodle/users/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.views.decorators.csrf import requires_csrf_token
 from .forms import UserRegisterForm
 
 
@@ -10,8 +8,7 @@ def register(request):
             if form.is_valid():
                 form.save()
                 username = form.cleaned_data.get('username')     
-                messages.success(request, f'Account created for {username}!')
-                return redirect('home.html')
+                return redirect('home')
 
       
     else:
