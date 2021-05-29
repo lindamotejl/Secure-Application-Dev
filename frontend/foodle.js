8ec6c1e59c66246b9862bb4c85ca2e7d43410171
SamuelM <samuel.mathew@praguecollege.cz>
2021-05-29 15:16:18 +0200

    Changes to filters
    Made cuisine filter work, others not clickable

--- frontend/foodle.js
+++ frontend/foodle.js
@@ -6,29 +6,19 @@ $(document).ready(function () {
         $('nav').toggleClass('show');
     });
 
-    // var header = document.getElementById("myDIV");
-    // var btns = header.getElementsByClassName("btn");
-    // for (var i = 0; i < btns.length; i++) {
-    //     btns[i].addEventListener("click", function() {
-    //     var current = document.getElementsByClassName("active");
-    //     if (current.length > 0) { 
-    //         current[0].className = current[0].className.replace(" active", "");
-    //     }
-    //     this.className += " active";
-    //     });
-    // }
+
     
 
 });
 
-$(document).ready(function() {
-    const btn = document.querySelectorAll(".btn");
-	btn.forEach(btn => {
-  btn.addEventListener('click',  function() {
-    btn.classList.toggle('orange');
-  });
-});
- });
+// $(document).ready(function() {
+//     const btn = document.querySelectorAll(".btn");
+// 	btn.forEach(btn => {
+//   btn.addEventListener('click',  function() {
+//     btn.classList.toggle('orange');
+//   });
+// });
+//  });
 
 //  $(document).ready(function() {
 //     const heart = document.querySelectorAll(".fa-heart");
